97df4b1d7676e267de9931ce7a974e01a5efd8d0
Matthew <matthew.everitt@viaccess-orca.com>
2023-08-12 08:42:12 +0300

    update linting and ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  'parser': '@typescript-eslint/parser',
+  'plugins': ['@typescript-eslint', 'react', 'react-hooks'],
+  'rules': {
+    // 'semi': ['error', 'never'],
+    'quotes': [2, 'single']
+  }
+}
--- src/redux/trips/selectors/selectSelf.ts
+++ src/redux/trips/selectors/selectSelf.ts
@@ -1,3 +1,3 @@
-import { RootState } from "../../store/store";
+import { RootState } from '../../store/store';
 
 export const selectSelf = (state: RootState) => state.trips;
